980ff174245bbf457180019a8cf87177dd229d59
adasgt <adas@dnanexus.com>
2015-12-30 08:03:30 +0000

    A test

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mylib-test',
+      version='0.1',
+      description='A test hello world library',
+      url='',
+      author='AD',
+      author_email='ad@example.com',
+      license='Y',
+      packages=['myapp'],
+      zip_safe=False)
